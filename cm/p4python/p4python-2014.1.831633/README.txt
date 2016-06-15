5866665049dbf1eab9e996fe1f335db71af896df
Brian Soumakian <bsoumakian@gmail.com>
2016-06-15 03:33:37 -0700

    remove scripts

--- cm/p4python/p4python-2014.1.831633/README.txt
+++
@@ -1,5 +0,0 @@
-This is P4Python
-################
-
-Please see RELNOTES.txt for hints how to build and use P4Python and for a 
-list of changes.
--- scripts/SyncTestSite.ps1
+++
@@ -1,6 +0,0 @@
-while ($true)
-{
-	Start-Sleep -Seconds 5
-	
-	CMD /C '""C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe" /command:update /path:"E:\sandboxes\bottletrade-autosync" /closeonend:1"'
-}
